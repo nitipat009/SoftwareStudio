b682655fa9e2819426f0e0698de887fa0a41bbdc
Gun Maksapsin <gun_102544@hotmail.com>
2022-02-27 15:01:39 +0700

    Update App.js

--- Group Week 5/react-part-1/src/App.js
+++ Group Week 5/react-part-1/src/App.js
@@ -12,7 +12,7 @@ function App() {
       id: 62010052,
       description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. A optio, molestias tempora quisquam explicabo consectetur? Corporis amet nulla consectetur numquam architecto ratione a libero! Debitis alias itaque, commodi maxime voluptas sit provident at aliquid cupiditate, explicabo molestiae eveniet obcaecati, ipsa tempora! Laboriosam, officiis ab? Sit, dolor laboriosam deleniti reprehenderit dolorem praesentium velit eaque nemo cumque asperiores debitis, perferendis nam non perspiciatis soluta eum aliquid odit est ipsam delectus quis? Cum recusandae quaerat id ea laboriosam pariatur consequuntur quos quas laudantium.',
       image:
-        "https://images.unsplash.com/photo-1634926878768-2a5b3c42f139?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=756&q=80",
+        "https://cdn.discordapp.com/attachments/802277621772714004/936911819627438120/unknown.png",
     },
     {
       firstname: "Nathamon",
