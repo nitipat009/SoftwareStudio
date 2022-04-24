dfb379a85e3b1548bd6b7f9fe3a9142a8be7afd7
nathamon <kaopod2000@gmail.com>
2022-04-24 15:22:30 +0700

    Update blog!

--- frontend/src/components/BlogCard.jsx
+++ frontend/src/components/BlogCard.jsx
@@ -1,37 +1,101 @@
 import { useNavigate } from "react-router-dom";
+import { format } from "date-fns";
 const BlogCard = (props) => {
-    const navigate = useNavigate();
+  const navigate = useNavigate();
 
-    const onClickHandler = () => {
-        navigate("/id")// id blog
-    }
-    
-    return (
-      <div class="max-w-sm rounded overflow-hidden shadow-lg hover:shadow-2xl h-72" onClick={onClickHandler}>
-          <img
-            class="w-full object-cover h-1/2"
-            src={props.url}
-            alt="Sunset in the mountains"
-          />
-        <div class="px-6 py-4 h-1/2">
-          <div class="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p class="text-gray-700 text-base">
-            Author: Kaopod
-          </p>
-        </div>
-        {/* <div class="px-6 pt-4 pb-2">
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #photography
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #travel
-          </span>
-          <span class="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
-            #winter
-          </span>
-        </div> */}
-      </div>
-    );
+  const onClickHandler = () => {
+    navigate("/id"); // id blog
   };
-  
-  export default BlogCard;
+
+  // Date formatter
+  const longEnUSFormatter = new Intl.DateTimeFormat("en-US", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+
+  return (
+    <div
+      className={`max-w-md rounded overflow-hidden shadow-lg hover:shadow-2xl h-96 relative ${
+        props.isHidden ? "hidden" : null
+      }`}
+      onClick={onClickHandler}
+    >
+      <div className="relative h-4/6">
+        <img
+          className="object-cover w-full h-full"
+          src={props.img}
+          alt="Flower and sky"
+        />
+        <div className="absolute top-0 right-0 ">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mx-3 my-3 flex items-center">
+            <svg
+              class="h-4 w-4 text-red-500 inline-block mr-1 fill-red-500"
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+            >
+              {" "}
+              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+            </svg>
+            {props.likes}
+          </span>
+        </div>
+        <div className="absolute bottom-0 left-0 px-5 py-1">
+          <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
+            #{props.tag}
+          </span>
+        </div>
+      </div>
+
+      <div className="px-6 py-4 h-1/6">
+        <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
+        <div className="text-gray-700 text-base pr-4">
+          by {props.username}{" "}
+          <div className="inline-block absolute right-0">
+            <div className="inline-block pr-3">
+              <svg
+                className="h-4 w-4 text-gray-400 inline-block"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                {" "}
+                <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />{" "}
+                <line x1="16" y1="2" x2="16" y2="6" />{" "}
+                <line x1="8" y1="2" x2="8" y2="6" />{" "}
+                <line x1="3" y1="10" x2="21" y2="10" />
+              </svg>
+
+              <a className="pl-1">{longEnUSFormatter.format(props.date)}</a>
+            </div>
+            <div className="inline-block pr-3">
+              <svg
+                class="inline-block h-5 w-5 text-gray-400"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M8 10h.01M12 10h.01M16 10h.01M9 16H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-5l-5 5v-5z"
+                />
+              </svg>
+              <a className="pl-1">{props.comments.length}</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default BlogCard;
--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,20 +1,164 @@
-import React from "react";
-import BlogCard from '../components/BlogCard.jsx'
-function Blog () {
+import { useState } from "react";
+import BlogCard from "../components/BlogCard.jsx";
+function Blog() {
+  const [bloglist, setBloglist] = useState([
+    {
+      id: 1,
+      username: "a",
+      tag: "Bangkok",
+      img: "https://images.unsplash.com/photo-1650582666851-359719341bb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      isHidden: false,
+      date: new Date(),
+      comments: [
+        {
+          id: "786",
+          username: "Coffee",
+          blogId: 1,
+          message:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: false,
+          date: new Date(),
+          likes: 3,
+        },
+        {
+          id: "456",
+          username: "kp",
+          blogId: 1,
+          message: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: true,
+          date: new Date(),
+          likes: 2,
+        },
+      ],
 
-    return (
-        
-          <div className="grid grid-cols-3 gap-4 w-2/4 mx-auto py-5 pt-[10em]">
-        <BlogCard url="https://images.unsplash.com/photo-1650582666851-359719341bb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
-        <BlogCard url="https://images.unsplash.com/photo-1648737155328-0c0012cf2f20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
-        <BlogCard url="https://images.unsplash.com/photo-1620912909703-6d16f0bf0412?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"/>
-        <BlogCard url="https://images.unsplash.com/photo-1650582666851-359719341bb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80"/>
-        <BlogCard url="https://images.unsplash.com/photo-1648737155328-0c0012cf2f20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80"/>
-        <BlogCard url="https://images.unsplash.com/photo-1620912909703-6d16f0bf0412?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80"/>  
-        </div>
-        
-      
-    );
-
+      likes: 10,
+    },
+    {
+      id: 2,
+      username: "a",
+      tag: "Bangkok",
+      img: "https://images.unsplash.com/photo-1648737155328-0c0012cf2f20?ixlib=rb-1.2.1&ixid=MnwxMjA3fDF8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80",
+      isHidden: false,
+      date: new Date(),
+      comments: [
+        {
+          id: "786",
+          username: "Coffee",
+          blogId: 2,
+          message:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: false,
+          date: new Date(),
+          likes: 3,
+        },
+        {
+          id: "456",
+          username: "kp",
+          blogId: 2,
+          message: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: true,
+          date: new Date(),
+          likes: 2,
+        },
+      ],
+      likes: 10,
+    },
+    {
+      id: 3,
+      username: "a",
+      tag: "Bangkok",
+      img: "https://images.unsplash.com/photo-1650582666851-359719341bb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      isHidden: false,
+      date: new Date(),
+      comments: [
+        {
+          id: "786",
+          username: "Coffee",
+          blogId: 2,
+          message:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: false,
+          date: new Date(),
+          likes: 3,
+        },
+        {
+          id: "456",
+          username: "kp",
+          blogId: 2,
+          message: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: true,
+          date: new Date(),
+          likes: 2,
+        },
+      ],
+      likes: 10,
+    },
+    {
+      id: 4,
+      username: "a",
+      tag: "Bangkok",
+      img: "https://images.unsplash.com/photo-1620912909703-6d16f0bf0412?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1169&q=80",
+      isHidden: true,
+      date: new Date(),
+      comments: [
+        {
+          id: "786",
+          username: "Coffee",
+          blogId: 2,
+          message:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: false,
+          date: new Date(),
+          likes: 3,
+        },
+        {
+          id: "456",
+          username: "kp",
+          blogId: 2,
+          message: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: true,
+          date: new Date(),
+          likes: 2,
+        },
+      ],
+      likes: 20,
+    },
+    {
+      id: 5,
+      username: "a",
+      tag: "Bangkok",
+      img: "https://images.unsplash.com/photo-1650582666851-359719341bb9?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80",
+      isHidden: false,
+      date: new Date(),
+      comments: [
+        {
+          id: "786",
+          username: "Coffee",
+          blogId: 2,
+          message:" Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: false,
+          date: new Date(),
+          likes: 3,
+        },
+        {
+          id: "456",
+          username: "kp",
+          blogId: 2,
+          message: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, dolores?",
+          isHidden: true,
+          date: new Date(),
+          likes: 2,
+        },
+      ],
+      likes: 10,
+    },
+  ]);
+console.log(bloglist)
+  let blog;
+  blog = bloglist.map((e) => {
+    return <BlogCard {...e} key={e.id}></BlogCard>;
+  });
+  console.log(bloglist);
+  return (
+    <div className="grid grid-cols-3 gap-6 w-3/4 mx-auto py-5 pt-[10em]">
+      {blog}
+    </div>
+  );
 }
-export default Blog
+export default Blog;
