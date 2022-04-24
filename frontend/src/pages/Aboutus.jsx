a9762bf1a17692e3c8be73118f0e76de004ae6cd
nathamon <kaopod2000@gmail.com>
2022-04-25 03:25:35 +0700

    Update

--- frontend/src/pages/Aboutus.jsx
+++ frontend/src/pages/Aboutus.jsx
@@ -2,7 +2,86 @@ import React from "react";
 function Aboutus () {
 
     return (
-        <div>This's Aboutus</div>
+        
+            <div class="max-w-screen-xl px-4 mx-auto md:px-8 pb-5">
+                <div className="h-[9em]"></div>
+        <div class="mb-10 md:mb-16">
+          <h2 class="text-2xl font-bold text-center mb-4">Our Team</h2>
+      
+          <p class="max-w-screen-md mx-auto text-center text-gray-500 md:text-lg">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis perspiciatis omnis aspernatur impedit vel, consectetur laudantium nulla et aliqua</p>
+        </div>
+      
+        <div class="grid gap-4 md:grid-cols-3">
+          <div class="p-4 shadow">
+            <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+              <img src="https://cdn.pixabay.com/photo/2016/11/21/12/42/beard-1845166__340.jpg" alt="Image" class="object-cover object-center w-full h-full" />
+            </div>
+      
+            <div class="flex flex-col items-center justify-center">
+              <div class="font-bold text-indigo-500 md:text-lg">John Doe</div>
+              <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">Founder / CEO</p>
+      
+            
+            </div>
+          </div>
+          <div class="p-4 shadow">
+            <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+              <img src="https://cdn.pixabay.com/photo/2016/11/18/19/07/happy-1836445__340.jpg" alt="Image" class="object-cover object-center w-full h-full" />
+            </div>
+      
+            <div class="flex flex-col items-center justify-center">
+              <div class="font-bold text-indigo-500 md:text-lg">John Doe</div>
+              <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">Web Developer</p>
+      
+            </div>
+          </div>
+          <div class="p-4 shadow">
+            <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+              <img src="https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401__340.jpg" alt="Image" class="object-cover object-center w-full h-full" />
+            </div>
+      
+            <div class="flex flex-col items-center justify-center">
+              <div class="font-bold text-indigo-500 md:text-lg">Scarlet</div>
+              <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">Devops Engineer</p>
+      
+            </div>
+          </div>
+          <div class="p-4 shadow">
+            <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+              <img src="https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401__340.jpg" alt="Image" class="object-cover object-center w-full h-full" />
+            </div>
+      
+            <div class="flex flex-col items-center justify-center">
+              <div class="font-bold text-indigo-500 md:text-lg">Scarlet</div>
+              <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">Devops Engineer</p>
+      
+            </div>
+          </div>
+          <div class="p-4 shadow">
+            <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+              <img src="https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401__340.jpg" alt="Image" class="object-cover object-center w-full h-full" />
+            </div>
+      
+            <div class="flex flex-col items-center justify-center">
+              <div class="font-bold text-indigo-500 md:text-lg">Scarlet</div>
+              <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">Devops Engineer</p>
+      
+            </div>
+          </div>
+          <div class="p-4 shadow">
+            <div class="h-48 mb-2 overflow-hidden rounded-lg shadow-lg md:h-80">
+              <img src="https://cdn.pixabay.com/photo/2018/01/15/07/52/woman-3083401__340.jpg" alt="Image" class="object-cover object-center w-full h-full" />
+            </div>
+      
+            <div class="flex flex-col items-center justify-center">
+              <div class="font-bold text-indigo-500 md:text-lg">Scarlet</div>
+              <p class="mb-3 text-sm text-gray-500 md:text-base md:mb-4">Devops Engineer</p>
+      
+            </div>
+          </div>
+        </div>
+      </div>
+      
     );
 
 }
--- frontend/src/pages/OneBlogEasy.jsx
+++ frontend/src/pages/OneBlogEasy.jsx
@@ -25,7 +25,7 @@ export default function OneBlogEasy() {
     title: "",
     info: "",
     img: "",
-    date: ""
+    date: "",
   });
   const handleData = (text) => (e) => {
     setData({ ...data, [text]: e.target.value });
@@ -46,59 +46,91 @@ export default function OneBlogEasy() {
     data.img = res.data.img;
     data.date = res.data.date;
   };
+
+  const likeHandler = () => {
+    
+    if (isLike){
+      setLike((state) => {return state-1})
+    }
+    if (!isLike){
+      setLike((state) => {return state+1})
+    }
+    setIsLike((state) => {return !state})
+    // do something here
+    // const res = await axios.put(`{PORT}/api/Comments/hide/${props.id}`, objUser);
+    // console.log(res);
+    
+  };
+  const [isLike,setIsLike] = useState(true);
+  const [isEdit, setIsEdit] = useState(false);
+  const [like,setLike] = useState(100);
   return (
-    <div className="flex flex-col h-full w-full">
+    <div className="flex flex-col items-center w-full ">
+      <div className="h-[9em]"></div>
       {/* Section Create Blog */}
-      <section className="mt-16 h-full justify-center p-16">
-        <h1>{data.title}</h1>
-        <h3>
-          Author : {data.username} Date : {data.date}
-        </h3>
-        <div className="flex flex-col w-full h-screen  justify-center items-center ">
+      <section className="w-[650px] ">
+        <div className="flex items-center justify-between">
+          <div className="font-bold text-4xl">Hello{data.title}</div>
+          <div className="flex items-center">
+            <span className="px-2 text-xl">
+              100
+              {/* {data.likes} */}
+            </span>
+            <svg
+              class={`h-10 w-10 text-red-500 hover:text-red-800 cursor-pointer ${isLike ? "fill-red-500 hover:text-red-300" : null}`}
+              viewBox="0 0 24 24"
+              fill="none"
+              stroke="currentColor"
+              stroke-width="2"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              onClick={likeHandler}
+            >
+              <path d="M20.84 4.61a5.5 5.5 0 0 0-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 0 0-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 0 0 0-7.78z" />
+            </svg>
+          </div>
+        </div>
+        
+        
+        <div className="text-xl mt-2 flex items-center">
+          Author : dsdfsdfdsf{data.username}
+          <svg
+            className="h-4 w-4 text-gray-800 inline-block ml-3 mr-1"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            stroke-width="2"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            <rect x="3" y="4" width="18" height="18" rx="2" ry="2" />
+            <line x1="16" y1="2" x2="16" y2="6" />
+            <line x1="8" y1="2" x2="8" y2="6" />
+            <line x1="3" y1="10" x2="21" y2="10" />
+          </svg>
+          sdfsdfdsfdsf
+          {data.date}
+        </div>
+        <div className="">
           {/* Image */}
-          <label class="flex flex-col w-full h-64 mt-16">
-            <div class="flex flex-col items-center justify-center pt-7">
+          <label class="">
+            <div class="">
               <img
                 id="preview"
-                className="object-cover rounded w-1/2 h-96"
-                src={data.img}
+                className="my-5 rounded-3xl w-full max-h-[400px] object-cover"
+                // src={data.img}
+                src="https://images.unsplash.com/photo-1650810287651-ce0e6418b997?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"
               />
             </div>
           </label>
           {/* Title */}
-          <div className="flex flex-col w-full h-screen ">
-            <div className="flex flex-row mt-16 items-center gap-x-2 ">
-              <h4 className="font-normal">Title</h4>
-              <div>
-                <input
-                  type={"text"}
-                  value={data.title}
-                  onChange={handleData("title")}
-                  className="p-2 border-b-2 border-black required:border-rose-500 required:rounded bg-transparent"
-                />
-              </div>
-            </div>
+          <div className="">
+            <div className=""></div>
             {/* Info */}
-            <div className="flex flex-row items-center gap-x-2">
-              <h4 className="font-normal">Info</h4>
-              <div>
-                <textarea
-                  type={"text"}
-                  id="role"
-                  value={data.info}
-                  className="p-16 border-b-2 border-black required:border-rose-500 required:rounded bg-transparent"
-                  onChange={handleData("info")}
-                />
-              </div>
+            <div className="indent-6 text-justify">
+              <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate veritatis sapiente quam provident ipsa consectetur cum fugit deserunt facilis, quaerat quos, animi nihil rem voluptatibus illo in aperiam? Officiis, quibusdam. Modi fugiat pariatur vel eligendi at eius est similique officiis saepe laudantium. Culpa veritatis consequatur itaque, incidunt vero reprehenderit mollitia. Aliquid rem soluta veritatis labore! Error dolore numquam voluptatum, aut quas ipsum cumque. Fugiat aut enim impedit similique qui blanditiis quos, sit temporibus dicta natus? Cumque inventore aliquam tempora pariatur! Perspiciatis, soluta. Dolor, laudantium cum? Adipisci quaerat officia, hic recusandae animi iste vitae. Voluptatibus perspiciatis eaque deleniti aliquam quae accusantium minus, voluptas beatae, explicabo eligendi debitis, possimus iure? Nihil magnam sunt ratione, adipisci aliquam architecto commodi laudantium sint consectetur. Quis laudantium sequi ipsa magni at officia commodi dignissimos similique? Ratione debitis perspiciatis dolorum dolor provident quisquam expedita iste cupiditate asperiores. Praesentium cumque dolorem itaque reiciendis at error reprehenderit nulla, id iste omnis ab quibusdam minima pariatur mollitia repellat perspiciatis nihil accusantium quis recusandae autem debitis maiores eveniet! Dolores illo, officia voluptate doloribus harum delectus! At, quam illum! Suscipit molestiae velit nostrum quas quisquam ratione mollitia voluptatem numquam tempore minima nesciunt temporibus odio itaque, voluptate ut officia, delectus illum atque! Asperiores.{data.info}</div>
             </div>
             {/* Submit Button */}
-            <button
-              id="button"
-              type="button"
-              class="bg-orange hover:bg-opacity-60 active:hover:bg-opacity-40 rounded text-3xl font-bold text-white mt-5"
-            >
-              Save
-            </button>
           </div>
         </div>
       </section>
